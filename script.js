9bcf6b3a8eacc7d8e6435f37e704e278f105d458
OverlordDev <gajdajila2@gmail.com>
2025-02-26 01:13:49 +0300

    Update

---
+++ script.js
@@ -0,0 +1,25 @@
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://cxebkrlkhzlymzkafvdh.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+function openModal(image, title, text, author, editor, date) {
+    const modal = document.getElementById('newsModal');
+    document.getElementById('modalImage').src = image;
+    document.getElementById('modalTitle').innerText = title;
+    document.getElementById('modalText').innerText = text;
+    document.getElementById('modalAuthor').innerText = author;
+    document.getElementById('modalEditor').innerText = editor;
+    document.getElementById('modalDate').innerText = date;
+
+    modal.classList.add('show');
+    modal.classList.remove('hide'); // Убираем класс скрытия, если он был
+    }
+
+    function closeModal() {
+        const modal = document.getElementById('newsModal');
+        modal.classList.add('hide');
+        setTimeout(() => {
+            modal.classList.remove('show', 'hide');
+        }, 300); // Ожидание завершения анимации перед скрытием
+    }
